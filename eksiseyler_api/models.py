2c00959090011cdc27c1cf982ad9f6e6acb8785f
frknltrk <unluturkfurkan@proton.me>
2024-07-22 19:40:05 +0300

    add annotations

--- eksiseyler_api/models.py
+++ eksiseyler_api/models.py
@@ -7,4 +7,5 @@ class Article(BaseModel):
     article_url: str
     article_title: str
     cover_image: str
-    article_date: Optional[str]
+    article_date: Optional[str]  # 'str | none' only for python>=3.10
+    # https://stackoverflow.com/a/76712732/14997609
