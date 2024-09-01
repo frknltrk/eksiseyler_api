d65b6f3adb3a6cb5aa04bc3f031b29f586ed59d1
frknltrk <unluturkfurkan@proton.me>
2024-09-01 21:29:37 +0300

    implement logging

--- etl/etl.py
+++ etl/etl.py
@@ -1,39 +1,63 @@
 import sqlite3
+import logging
 from scraper import scrape_articles
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def setup_database():
-    conn = sqlite3.connect("data/eksiseyler.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS articles (
-                    article_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    article_url TEXT NOT NULL UNIQUE,
-                    article_title TEXT,
-                    cover_image TEXT,
-                    article_date DATE
-                  )"""
-    )
-    conn.commit()
-    conn.close()
+    logging.info("Setting up the database.")
+    try:
+        conn = sqlite3.connect("data/eksiseyler.db")
+        cursor = conn.cursor()
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS articles (
+                        article_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        article_url TEXT NOT NULL UNIQUE,
+                        article_title TEXT,
+                        cover_image TEXT,
+                        article_date DATE
+                      )"""
+        )
+        conn.commit()
+        logging.info("Database setup complete.")
+    except sqlite3.Error as e:
+        logging.error(f"Error setting up database: {e}")
+    finally:
+        conn.close()
 
 
 def insert_articles(articles):
-    conn = sqlite3.connect("data/eksiseyler.db")
-    cursor = conn.cursor()
-    cursor.executemany(
-        """INSERT OR IGNORE INTO articles (article_url, article_title, cover_image)
-                      VALUES (?, ?, ?)""",
-        reversed(articles),
-    )
-    conn.commit()
-    conn.close()
+    logging.info(f"Inserting {len(articles)} articles into the database.")
+    logging.debug(f"Articles to be inserted: {articles}")
+    try:
+        conn = sqlite3.connect("data/eksiseyler.db")
+        cursor = conn.cursor()
+        cursor.executemany(
+            """INSERT OR IGNORE INTO articles (article_url, article_title, cover_image)
+                          VALUES (?, ?, ?)""",
+            reversed(articles),
+        )
+        conn.commit()
+        logging.info("Articles inserted successfully.")
+    except sqlite3.Error as e:
+        logging.error(f"Error inserting articles: {e}")
+    finally:
+        conn.close()
 
 
 def run_etl():
+    logging.info("Starting ETL process.")
     setup_database()
-    articles = scrape_articles()
-    insert_articles(articles)
+    try:
+        articles = scrape_articles()
+        insert_articles(articles)
+    except Exception as e:
+        logging.error(f"ETL process failed: {e}")
+    logging.info("ETL process completed.")
 
 
 if __name__ == "__main__":
--- etl/scraper.py
+++ etl/scraper.py
@@ -1,31 +1,45 @@
 import requests
+import logging
 from bs4 import BeautifulSoup
+
+# Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def scrape_articles():
     url = "https://eksiseyler.com/"
-    h = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
-    }
-    response = requests.get(url, headers=h)
-    soup = BeautifulSoup(response.content, "lxml")
-    articles = []
+    logging.info(f"Scraping articles from {url}")
+    try:
+        h = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0"
+        }
+        response = requests.get(url, headers=h)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.content, "lxml")
+        articles = []
 
-    divs = soup.find_all("div", class_=["content-img", "mashup-img"])
+        divs = soup.find_all("div", class_=["content-img", "mashup-img"])
 
-    articles = []
-    for div in divs:
-        url = div.a["href"]
-        title = div.img["alt"]
-        img = div.img.get("data-src") or div.img.get("src")  # content-img or mashup-img
+        for div in divs:
+            url = div.a["href"]
+            title = div.img["alt"]
+            img = div.img.get("data-src") or div.img.get(
+                "src"
+            )  # content-img or mashup-img
 
-        # as tuples:
-        articles.insert(0, (url, title, img))
-        # as dict items:
-        # parsed_dict.append({
-        #     'article_url': url,
-        #     'article_title': title,
-        #     'cover_image': img
-        # })
+            # as tuples:
+            articles.insert(0, (url, title, img))
+            # as dict items:
+            # parsed_dict.append({
+            #     'article_url': url,
+            #     'article_title': title,
+            #     'cover_image': img
+            # })
 
-    return articles
+        logging.info(f"Scraped {len(articles)} articles.")
+        return articles
+    except requests.RequestException as e:
+        logging.error(f"Error scraping articles: {e}")
+        return []
