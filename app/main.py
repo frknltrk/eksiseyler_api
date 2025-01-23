45af14401f0cc78f97e4b58faf6367fa30c0b6da
frknltrk <unluturkfurkan@proton.me>
2025-01-23 17:05:06 +0300

    change type of scraped_at to datetime

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-import os
+import os, datetime, psycopg_pool
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List
-import psycopg_pool
 
 app = FastAPI()
 
@@ -15,31 +14,50 @@ pool = psycopg_pool.ConnectionPool(
     timeout=10,  # Adjust timeout to handle slow startups
 )
 
+
 class Article(BaseModel):
     article_url: str
     article_title: str
     cover_image: str
-    scraped_at: str
+    scraped_at: datetime.datetime
+
 
 def get_db():
     with pool.connection() as conn:
         yield conn
 
+
 @app.get("/articles/all", response_model=List[Article])
 def read_all_articles(db=Depends(get_db)):
     with db.cursor() as cur:
-        cur.execute("SELECT article_url, article_title, cover_image, scraped_at FROM articles")
+        cur.execute(
+            "SELECT article_url, article_title, cover_image, scraped_at FROM articles"
+        )
         rows = cur.fetchall()
-    return [Article(article_url=row[0], article_title=row[1], cover_image=row[2], scraped_at=row[3]) for row in rows]
+    return [
+        Article(
+            article_url=row[0],
+            article_title=row[1],
+            cover_image=row[2],
+            scraped_at=row[3],
+        )
+        for row in rows
+    ]
+
 
 @app.get("/articles/random", response_model=Article)
 def read_random_article(db=Depends(get_db)):
     with db.cursor() as cur:
-        cur.execute("SELECT article_url, article_title, cover_image, scraped_at FROM articles ORDER BY RANDOM() LIMIT 1")
+        cur.execute(
+            "SELECT article_url, article_title, cover_image, scraped_at FROM articles ORDER BY RANDOM() LIMIT 1"
+        )
         row = cur.fetchone()
     if row is None:
         raise HTTPException(status_code=404, detail="No articles found")
-    return Article(article_url=row[0], article_title=row[1], cover_image=row[2], scraped_at=row[3])
+    return Article(
+        article_url=row[0], article_title=row[1], cover_image=row[2], scraped_at=row[3]
+    )
+
 
 @app.get("/health")
 def health_check():
@@ -50,4 +68,3 @@ def health_check():
         return {"status": "healthy"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
-
